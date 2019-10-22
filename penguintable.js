1230e3c023fb2f210e752932d0f076bc3e8d4284
J Ross <j.ross@centre.edu>
2019-10-22 10:53:12 -0400

    updated

--- penguintable.js
+++ penguintable.js
@@ -1,1 +1,43 @@
 
+
+tablepromise = d3.json("classData.json")
+
+tablepromise.then(
+
+function(data)
+    {
+        maketable(data);
+        console.log("good", (data))
+        
+        
+    }
+
+
+
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+var maketable = function (data)
+{
+d3.select("#table")
+.append("table")
+.attr( "id" ,"#table")
+d3.select("#table")
+.selectAll("tr")
+.data(data)
+.enter()
+.append("tr")
+}
+
